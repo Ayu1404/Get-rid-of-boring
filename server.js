eeec95f52394204289301f2a38c19b7346153021
unknown <Ayush Sharma>
2024-08-17 19:17:21 +0530

    Resolved merge conflicts

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express();
-const port = 6001;
+const port = 7001;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
